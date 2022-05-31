30461085a09224f01f014ae2d89362dde56d6ca1
whynew.in <whynew.in@DESKTOP-G9DF0PR>
2022-05-31 12:20:21 +0530

    22222

---
+++ src/test/java/practice2/PracticePipeline2Test.java
@@ -0,0 +1,19 @@
+package practice2;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class PracticePipeline2Test {
+	
+	
+	@Test()
+
+	public void practice1Test() {
+		
+
+		Reporter.log("hello", true);
+
+
+	}
+
+}
